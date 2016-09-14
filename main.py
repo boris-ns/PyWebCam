b3c8861b73572e0d9c32c127e3203137a0965519
boris-ns <b.sulicenko@gmail.com>
2016-09-14 11:43:17 +0200

    Added functionality to take a picture from video

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from threading import Thread
 
 cap = cv2.VideoCapture(0)
 EXIT = False
+TAKE_PICTURE = False
 command = ''
 
 def help_screen():
@@ -13,19 +14,29 @@ def help_screen():
     print("> takepic - Take picture")
     print("> exit - Exit the program\n")
 
+def shut_down():
+    cap.release()
+    cv2.destroyAllWindows()
+
+def take_picture(name, picture):
+    cv2.imwrite(name, picture)
+
 def command_line():
+    global TAKE_PICTURE, EXIT
+
     while True:
         command = input("> ")
 
         if command == 'help':
             help_screen()
-        elif command == 'exit': # TODO namesti exit da radi, problem je sto deo promena EXIT ne stigne do frame_loading
-            EXIT = True
-            print(EXIT)
+        elif command == 'exit': 
+            shut_down()
         elif command == 'takepic': # TODO prosledi nekako frame
-            cv2.imwrite('slika.png', frame)
+            TAKE_PICTURE = True
 
 def frame_loading():
+    global TAKE_PICTURE, EXIT
+
     command_line_thread = Thread(target=command_line)
     command_line_thread.daemon = True
     command_line_thread.start()
@@ -34,11 +45,13 @@ def frame_loading():
         ret, frame = cap.read()  
         cv2.imshow('frame', frame)            
 
+        if TAKE_PICTURE:
+            take_picture('slika.png', frame)
+            TAKE_PICTURE = False
+
         if cv2.waitKey(1) & 0xFF == ord('q') or EXIT:
+            shut_down()
             break
 
 
 frame_loading()
-
-cap.release()
-cv2.destroyAllWindows()
